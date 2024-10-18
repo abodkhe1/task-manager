c50330ba85be5f3eee8d3dd05c0d238df04da14d
abodkhe1 <122731356+abodkhe1@users.noreply.github.com>
2024-10-18 15:54:03 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-// MongoDB Connection
 mongoose.connect('mongodb+srv://ajitbodkhe2017:FlyPKnR5DkwRsdqA@taskmanager.xsgn3.mongodb.net/taskManager?retryWrites=true&w=majority', {
-    useNewUrlParser: true,       // Use new URL string parser
-    useUnifiedTopology: true,    // Use new topology engine
-    serverSelectionTimeoutMS: 5000, // Adjust server selection timeout (default is 30,000ms)
-    connectTimeoutMS: 10000,     // Adjust connection timeout
-    socketTimeoutMS: 45000       // Adjust socket timeout (close idle connections after 45 seconds)
+    serverSelectionTimeoutMS: 5000, // Adjust server selection timeout
+    connectTimeoutMS: 10000,        // Adjust connection timeout
+    socketTimeoutMS: 45000          // Adjust socket timeout
 })
   .then(() => {
     console.log("Connected to MongoDB successfully");
@@ -15,5 +9,3 @@ mongoose.connect('mongodb+srv://ajitbodkhe2017:FlyPKnR5DkwRsdqA@taskmanager.xsgn
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
-
-module.exports = mongoose;
