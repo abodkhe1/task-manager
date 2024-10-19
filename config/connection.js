899f35123f2ec797d654f2cd81b34f6f8a0ac5cb
abodkhe1 <122731356+abodkhe1@users.noreply.github.com>
2024-10-19 14:12:18 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://ajitbodkhe2017:JY77QtyZSzx5uVMm@taskmanager.xsgn3.mongodb.net/taskManager', {
-  serverSelectionTimeoutMS: 10000, // Increase server selection timeout
-  connectTimeoutMS: 15000,         // Increase connection timeout
-  socketTimeoutMS: 60000           // Increase socket timeout
+  serverSelectionTimeoutMS: 10000, 
+  connectTimeoutMS: 15000,        
+  socketTimeoutMS: 60000           
 })
   .then(() => {
     console.log("Connected to MongoDB successfully");
